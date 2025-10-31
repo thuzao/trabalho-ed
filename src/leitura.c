b7941aebd7d3f31c5c536e8b7be320b37eb4e25a
arthur <marquesarthur92@gmail.com>
2025-10-30 23:32:12 -0300

    Commit 4 - Iniciando o scrip para leitura

--- src/leitura.c
+++ src/leitura.c
@@ -8,5 +8,14 @@
 #include "../includes/modelos/bd_partidas.h"
 
 void carregarTimes(const char bd_times, BD_Times *dados){
+    FILE *arquivo = fopen(bd_times, "r");
+    
+    //Verificação se o arquivo foi lido corretamente
+    if (arquivo == NULL){
+        printf("Erro ao abrir o arquivo %s \n", bd_times);
+    } else {
+
+        
+    }
 
 }
